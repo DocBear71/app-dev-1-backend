d1dc20ad0dd556b37daffe928e2c9340d5fbb62c
Edward Gene McKeown <edward@doctormckeown.com>
2026-02-25 11:49:41 -0600

    updating and removing yelp 3 02-25-2026

--- api/index.js
+++ api/index.js
@@ -11,8 +11,11 @@ connectAll();
 // Application setup
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://eds-nodejs25.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
